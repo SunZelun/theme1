2221315fac7176351d4cae53213211bba22fcbc7
Sun <sun.z@pg.com>
2018-08-24 14:28:40 +0800

    update mailer

--- send_mail.php
+++ send_mail.php
@@ -7,8 +7,6 @@ use PHPMailer\PHPMailer\Exception;
 //Load Composer's autoloader
 require 'vendor/autoload.php';
 
-var_dump("11212132");
-
 $mail = new PHPMailer(true);                              // Passing `true` enables exceptions
 try {
     //Server settings
@@ -17,7 +15,7 @@ try {
     $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
     $mail->SMTPAuth = true;                               // Enable SMTP authentication
     $mail->Username = 'sunzelun90@gmail.com';                 // SMTP username
-    $mail->Password = '901031Szl';                           // SMTP password
+    $mail->Password = '20151105Szl';                           // SMTP password
     $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
     $mail->Port = 587;                                    // TCP port to connect to
 
